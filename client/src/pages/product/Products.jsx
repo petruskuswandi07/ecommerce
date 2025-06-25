2bd2f4f98a115dba8d9c6458dadf26c9fe4fe140
petruskuswandi07 <petruskuswandi1@gmail.com>
2025-06-25 10:21:00 +0700

    add fitur minor show rating, name, and stock

--- client/src/pages/product/Products.jsx
+++ client/src/pages/product/Products.jsx
@@ -2,22 +2,39 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { Box, CardActionArea } from "@mui/material";
 import products from "../../data/Products";
+import StarRoundedIcon from "@mui/icons-material/StarRounded";
+import { orange } from "@mui/material/colors";
 
 const Products = () => {
   return (
     <>
       {products.map((product) => (
-        <Card key={product.name} sx={{ width: 210, height: 280 }}>
+        <Card key={product.name} sx={{ width: 210, minHeight: 280 }}>
           <CardActionArea>
             <CardMedia
               component="img"
               alt={product.name}
               image={product.image[0] ? product.image[0].link : null}
-              sx={{ minHeight: 200 }}
+              sx={{ minHeight: 200, objectFit: "cover" }}
             />
           </CardActionArea>
+          <CardContent>
+            <Typography>{product.name}</Typography>
+            <Typography fontWeight="bold">{`Rp${parseFloat(
+              product.price
+            ).toLocaleString("id-ID")}`}</Typography>
+
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography sx={{ display: "flex", alignItems: "center" }}>
+                <StarRoundedIcon sx={{ color: orange[500] }} /> {product.rating}
+              </Typography>
+              <Typography sx={{ display: "flex", alignItems: "center" }}>
+                stok: {product.stock}
+              </Typography>
+            </Box>
+          </CardContent>
         </Card>
       ))}
     </>
